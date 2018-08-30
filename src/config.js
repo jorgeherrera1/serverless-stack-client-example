22a190d17e65cd33c81b17ddff1b3adc8d0aa975
Jorge Herrera <jorge@Jorges-MacBook-Pro.local>
2018-08-29 22:27:46 -0500

    Set up AWS in client project

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-jh"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://9xp4hl4ivk.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_scAXWVzif",
+    APP_CLIENT_ID: "3k3f71m59b36fg0fj8qlb49a0b",
+    IDENTITY_POOL_ID: "us-east-1:900d6708-c2ed-47a6-8cdb-b1be409c03ad"
+  }
+};
