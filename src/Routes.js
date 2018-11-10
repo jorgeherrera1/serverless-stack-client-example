9a7bdc444ed1357c7b330590d07984c000ac2e35
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2018-11-10 12:10:13 -0600

    Displaying note form

--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
 import Signup from "./containers/SignUp";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 export default ({ childProps }) =>
   <Switch>
@@ -13,5 +14,6 @@ export default ({ childProps }) =>
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     <Route component={NotFound} />
   </Switch>;
